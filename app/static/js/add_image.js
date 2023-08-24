c50a0929ec801d04fe0b704bc4a88f41bb8237dc
ogzCode <oguzguc44@gmail.com>
2023-08-24 15:44:50 +0300

    Add Image page edited

--- app/static/js/add_image.js
+++ app/static/js/add_image.js
@@ -1,19 +1,5 @@
-function previewImage(input) {
-    var imagePreview = document.getElementById("imagePreview");
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            imagePreview.src = e.target.result;
-        };
-        reader.readAsDataURL(input.files[0]);
-        imagePreview.style.display = "block";
-    } else {
-        imagePreview.src = "#"; // Reset preview if no image is selected
-    }
-}
-
 window.addEventListener("load", function () {
     const navbarHeight = document.querySelector('.navbar').offsetHeight;
-    const formContainer = document.querySelector('.form__container');
+    const formContainer = document.querySelector('.container');
     formContainer.style.height = `calc(100vh - ${navbarHeight}px)`;
 });
